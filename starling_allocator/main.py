6c481dcdaa2e09cfa4e57b2c438976b939d39e52
Mickey Li <mickeyhli@outlook.com>
2021-09-02 18:20:37 +0100

    Initial commit

---
+++ starling_allocator/main.py
@@ -0,0 +1,11 @@
+
+
+import rclpy
+from rclpy.node import Node
+
+from starling_allocator_msgs.msg import Allocator
+from starling_allocator_msgs.srv import AllocateTrajectories
+
+
+def main(args=None):
+    pass

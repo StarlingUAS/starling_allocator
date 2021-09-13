bb7b5fda65cce709859eed79ee31ab3be80d15bb
Mickey Li <mickeyhli@outlook.com>
2021-09-13 15:39:48 +0100

    Updated CI

--- starling_allocator/starling_allocator/main.py
+++ starling_allocator/starling_allocator/main.py
@@ -17,6 +17,7 @@ class Allocator(Node):
     def __init__(self):
         super().__init__('starling_allocator')
         self.submit_trajectories_srv = self.create_service(AllocateTrajectories, 'submit_trajectories', self.submit_trajectories_cb)
+        self.get_logger().info("Initialised")
 
     def submit_trajectories_cb(self, req, res):
         self.get_logger().info("Trajectory Submit Request Received")
@@ -31,17 +32,22 @@ class Allocator(Node):
 
         # Do something with allocation method, currently just allocate by order
 
-        for name, traj in zip(current_namespace, req.trajectories):
-            cli = self.create_client(SubmitTrajectory, f'{name}/submit_trajectory')
+        for name, traj in zip(current_namespaces, req.trajectories):
+            srv_name = f'{name}/submit_trajectory'
+            cli = self.create_client(SubmitTrajectory, srv_name)
             sreq = SubmitTrajectory.Request()
             sreq.trajectory = traj
             sreq.type = 'position'
+            sreq.interpolation_method = 'cubic'
             sreq.auto_arm = True
+            sreq.auto_takeoff = True
 
             while not cli.wait_for_service(timeout_sec=1.0):
-                self.get_logger().info('service not available, waiting again...')
+                self.get_logger().info(f'service "{srv_name}" not available, waiting again...')
             cli.call_async(sreq)
-            self.get_logger().info(f"Sent trajectory request to vehicle {name}")
+            self.get_logger().info(f"Sent trajectory request to vehicle '{name}' via {srv_name}\n{sreq}")
+
+        return res
 
 
     def __get_current_vehicle_namespaces(self):
@@ -51,7 +57,10 @@ class Allocator(Node):
         for topic_name, _ in topic_list:
             self.get_logger().info(topic_name)
             if 'mavros' in topic_name:
-                namespaces.add(topic_name.split('/')[1])
+                name = topic_name.split('/')[1]
+                if name == 'mavros':
+                    name = ''
+                namespaces.add(name)
         self.get_logger().info(f'Found {len(namespaces)} namespaces: {",".join(namespaces)}')
         return namespaces
 
